d07db6a4f51eec0b88d50417602b26a793585a4e
sofiagen8 <c26sofia.gentile@afacademy.af.edu>
2026-03-12 16:16:33 -0600

    code finished

--- src/top_basys3.vhd
+++ src/top_basys3.vhd
@@ -86,12 +86,51 @@ end top_basys3;
 architecture top_basys3_arch of top_basys3 is 
   
 	-- declare components
-
+    component thunderbird_fsm is 
+      port(
+        i_clk, i_reset : in std_logic;
+        i_left, i_right : in std_logic;
+        o_lights_L : out  std_logic_vector(2 downto 0);
+        o_lights_R : out std_logic_vector(2 downto 0) --inside right taillight => RA=LSB of o_lights_R, RC=MSB
+        
+      );
+    end component thunderbird_fsm;
+  
+  component clock_divider is
+	generic ( constant k_DIV : natural := 2	); -- How many clk cycles until slow clock toggles
+											   -- Effectively, you divide the clk double this 
+											   -- number (e.g., k_DIV := 2 --> clock divider of 4)
+	port ( 	i_clk    : in std_logic;
+			i_reset  : in std_logic;		   -- asynchronous
+			o_clk    : out std_logic		   -- divided (slow) clock
+	);
+end component clock_divider;
+  
+  signal w_clk : std_logic; ---if no use then delete, just copying from ice4
   
 begin
 	-- PORT MAPS ----------------------------------------
-
+    thunderbird_fsm_instance: thunderbird_fsm port map(
+        i_right => sw(0),
+        i_left => sw(15),
+        i_clk => w_clk,
+        i_reset => btnR,
+        o_lights_L(0) => led(13),
+        o_lights_L(1) => led(14),
+        o_lights_L(2) => led(15),
+        o_lights_R(0) => led(2),
+        o_lights_R(1) => led(1),
+        o_lights_R(2) => led(0)
+    
+	);
 	
+	clkdiv_inst: clock_divider
+	   generic map( k_DIV => 50000000)
+	   port map(
+	       i_clk => clk,
+	       i_reset => btnL,
+	       o_clk => w_clk
+	   );
 	
 	-- CONCURRENT STATEMENTS ----------------------------
 	
