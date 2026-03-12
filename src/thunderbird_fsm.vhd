24104702d466dcb051c30b0c1fe6d4f4aeb71d86
sofiagen8 <c26sofia.gentile@afacademy.af.edu>
2026-03-11 21:48:48 -0600

    blinker maybe fixed

--- src/thunderbird_fsm.vhd
+++ src/thunderbird_fsm.vhd
@@ -112,9 +112,9 @@ begin
     o_lights_L(0) <= (f_Q(2) and not f_Q(1) and f_Q(0));
     o_lights_L(1) <= (f_Q(2) and f_Q(1) and not f_Q(0));
     o_lights_L(2) <= (f_Q(2) and f_Q(1) and f_Q(0));
-    o_lights_R(0) <= (not f_Q(2) and f_Q(1) and not f_Q(0));
+    o_lights_R(2) <= (not f_Q(2) and f_Q(1) and not f_Q(0));
     o_lights_R(1) <= (not f_Q(2) and f_Q(1) and f_Q(0));
-    o_lights_R(2) <= (f_Q(2) and not f_Q(1) and not f_Q(0));
+    o_lights_R(0) <= (f_Q(2) and not f_Q(1) and not f_Q(0));
     
     ---------------------------------------------------------------------------------
 	
