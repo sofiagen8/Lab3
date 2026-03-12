2efec875f9059122909011c72aff5ec1e7e5b5ed
sofiagen8 <c26sofia.gentile@afacademy.af.edu>
2026-03-11 21:27:16 -0600

    trying

--- src/thunderbird_fsm_tb.vhd
+++ src/thunderbird_fsm_tb.vhd
@@ -57,24 +57,86 @@ end thunderbird_fsm_tb;
 architecture test_bench of thunderbird_fsm_tb is 
 	
 	component thunderbird_fsm is 
---	  port(
-		
---	  );
+    port(i_clk, i_reset : in std_logic;
+        i_left, i_right : in std_logic;
+        o_lights_L : out  std_logic_vector(2 downto 0);
+        o_lights_R : out std_logic_vector(2 downto 0) --inside right taillight => RA=LSB of o_lights_R, RC=MSB
+	  );
 	end component thunderbird_fsm;
 
 	-- test I/O signals
+	signal w_left : std_logic := '0';
+	signal w_right : std_logic := '0';
+	signal w_reset : std_logic := '0';
+	signal w_clk : std_logic := '0';
+	
+	--Outputs
+	signal w_blinker_R : std_logic_vector(2 downto 0) := "000";
+	signal w_blinker_L : std_logic_vector(2 downto 0) := "000";	
 	
 	-- constants
-	
+	constant k_clk_period : time := 10 ns;
 	
 begin
 	-- PORT MAPS ----------------------------------------
-	
+	uut: thunderbird_fsm port map(
+          i_reset => w_reset,
+          i_clk => w_clk,
+          i_left => w_left,
+          i_right => w_right,
+          o_lights_L => w_blinker_L, --no idea if this works, but come back to if logic/runtime errors
+          o_lights_R => w_blinker_R
+        );
 	-----------------------------------------------------
 	
 	-- PROCESSES ----------------------------------------	
     -- Clock process ------------------------------------
-    
+    clk_proc : process
+        begin
+            w_clk <= '0';
+            wait for k_clk_period/2;
+            w_clk <= '1';
+            wait for k_clk_period/2;
+        end process;
+	
+	-- Simulation process
+	-- Use 220 ns for simulation
+	sim_proc: process
+	begin --ALL TEST BENCHES BELOW ARE COPY/PASTED FROM ICE4
+		-- sequential timing		
+		w_reset <= '1';
+		wait for k_clk_period*1;
+		  assert w_stoplight = "010" report "bad reset" severity failure;
+		
+		w_reset <= '0';
+		wait for k_clk_period*1;
+		
+		-- red light
+		w_C <= '0'; wait for k_clk_period;
+          assert w_stoplight = "100" report "should be red when no car" severity failure;
+		-- car shows up at red light
+        w_C <= '1'; wait for k_clk_period;
+            assert w_stoplight = "001" report "should be green when car present" severity failure;
+        wait for k_clk_period * 3; -- stay green
+            assert w_stoplight = "001" report "should be green when car present" severity failure;
+        -- go to yellow
+        w_C <= '0'; wait for k_clk_period;
+            assert w_stoplight = "010" report "should be yellow when cars done" severity failure;
+        wait for k_clk_period; -- time to go to red
+            assert w_stoplight = "100" report "did not go red after yellow" severity failure;
+        
+        -- reset and test yellow to red even if car
+        w_reset <= '1'; w_C <= '1';
+            wait for k_clk_period;
+        w_reset <= '0';
+          assert w_stoplight = "010" report "bad reset" severity failure;
+        wait for k_clk_period;
+            assert w_stoplight = "100" report "skipped red after yellow" severity failure;
+        wait for k_clk_period;
+            assert w_stoplight = "001" report "should be green when car present" severity failure;
+	
+		wait;
+	end process;
 	-----------------------------------------------------
 	
 	-- Test Plan Process --------------------------------
